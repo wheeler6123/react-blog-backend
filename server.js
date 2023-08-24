9f31e8b90265747bb6ac607b08e4fa658032a9dd
John <wheeler6123@gmail.com>
2023-08-24 13:26:53 -0600

    set up mongoose, post schema and post models

--- server.js
+++ server.js
@@ -3,3 +3,31 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 
 require('dotenv').config();
+
+// set up express
+const app = express();
+
+// set up cors
+app.use(cors());
+
+// set up express to handle json
+app.use(express.json());
+
+// set up mongoose
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err.message));
+
+// set up schema
+const postSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    author: String,
+    content: String
+});
+
+// set up model
+const Post = mongoose.model('Post', postSchema);
+
+// set up controller
+
