a6a093e3f9f8a76c08c13cb1b658b7d78e6cca88
John <wheeler6123@gmail.com>
2023-08-26 13:32:11 -0600

    update posts controller code

--- server.js
+++ server.js
@@ -34,24 +34,24 @@ const Post = mongoose.model('Post', postSchema);
 // get all posts
 app.get('/posts', async (req, res) => {
     const posts = await Post.find();
-    res.json(posts);
+    res.send(posts);
 });
 
 // get one post
 app.get('/posts/:id', async (req, res) => {
     const post = await Post.findById(req.params.id);
-    res.json(post);
+    res.send(post);
 });
 
 // create post
 app.post('/posts', async (req, res) => {
     const post = new Post(req.body);
     await post.save();
-    res.json(post);
+    res.send(post);
 });
 
 // delete post
 app.delete('/posts/:id', async (req, res) => {
     const post = await Post.findByIdAndDelete(req.params.id);
-    res.json(post);
+    res.send(post);
 });
